5a5857e021569e0fd5f9539c688101d3e68f6536
digaprajogo <prajogodiga@gmail.com>
2018-11-05 11:08:25 +0700

    commit andi 1

---
+++ git2.js
@@ -0,0 +1,8 @@
+// tujuan menggunakan branch adalah untuk membuat orang lain mengerjakan sebagian dari code
+// branch master   > branch master
+//                 > branch 1 untuk dikerjakan andi
+//                 > branch 2 untuk dikerjakan budi
+// jika kerjaan budi salah masih bisa untuk di delete dan tidak perlu di merged ke branch utama
+
+var nama = 'Andi'
+console.log(nama)
